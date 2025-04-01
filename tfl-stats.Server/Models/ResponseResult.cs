fe728b14efd4850762d01de2b0a4f49a2809b3f6
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-01 20:22:34 +0100

    Refactor ResponseResult and update dependencies

--- tfl-stats.Server/Models/ResponseResult.cs
+++ tfl-stats.Server/Models/ResponseResult.cs
@@ -5,14 +5,14 @@
         public bool IsSuccessful { get; set; }
         public T? Data { get; set; }
         public ResponseStatus ResponseStatus { get; set; }
-        public string Message { get; set; }
+        //public string Message { get; set; }
 
-        public ResponseResult(bool isSuccessful, T? data, ResponseStatus responseStatus, string message)
+        public ResponseResult(bool isSuccessful, T? data, ResponseStatus responseStatus)
         {
             IsSuccessful = isSuccessful;
             Data = data;
             ResponseStatus = responseStatus;
-            Message = message;
+            //Message = message;
         }
     }
 
