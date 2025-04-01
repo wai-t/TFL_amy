57e913cb56b4d2b1e5252963ac7c41835c0e8215
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-01 20:21:41 +0100

    Refactor JourneyService to use ApiClient

--- tfl-stats.Server/Services/JourneyService/JourneyService.cs
+++ tfl-stats.Server/Services/JourneyService/JourneyService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using tfl_stats.Server.Configurations;
 using tfl_stats.Server.Models;
 using tfl_stats.Server.Models.JourneyModels;
@@ -9,17 +8,21 @@ namespace tfl_stats.Server.Services.JourneyService
 {
     public class JourneyService
     {
-        private readonly HttpClient _httpclient;
+        //private readonly HttpClient _httpclient;
+        private readonly ApiClient _apiclient;
+
         private ILogger<JourneyService> _logger;
 
         private readonly string appId;
         private readonly string appKey;
         private readonly string baseUrl;
 
-        public JourneyService(HttpClient httpClient,
+        public JourneyService(ApiClient apiClient,
             IOptions<AppSettings> options, ILogger<JourneyService> logger)
+
         {
-            _httpclient = httpClient;
+            //_httpclient = httpClient;
+            _apiclient = apiClient;
             _logger = logger;
             appId = options.Value.appId ?? throw new ArgumentNullException(nameof(appId));
             appKey = options.Value.appKey ?? throw new ArgumentNullException(nameof(appKey));
@@ -33,25 +36,24 @@ namespace tfl_stats.Server.Services.JourneyService
 
             if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to))
             {
-                return new ResponseResult<List<Journey>>(false, [], ResponseStatus.BadRequest, "Invalid stop points.");
+                return new ResponseResult<List<Journey>>(false, [], ResponseStatus.BadRequest);
             }
 
             string url = $"{baseUrl}Journey/journeyresults/{Uri.EscapeDataString(from)}/to/{Uri.EscapeDataString(to)}?app_id={appId}&app_key={appKey}";
-            var journeyResponse = await GetFromApi<JourneyResponse>(url, "getJourney");
+            var journeyResponse = await _apiclient.GetFromApi<JourneyResponse>(url, "getJourney");
 
             if (journeyResponse?.Journeys != null)
             {
-                return new ResponseResult<List<Journey>>(true, journeyResponse.Journeys, ResponseStatus.Ok, "Request Succeeded");
+                return new ResponseResult<List<Journey>>(true, journeyResponse.Journeys, ResponseStatus.Ok);
             }
 
-            return new ResponseResult<List<Journey>>(false, [], ResponseStatus.NotFound, "No journeys found in response.");
-
+            return new ResponseResult<List<Journey>>(false, [], ResponseStatus.NotFound);
         }
 
         private async Task<string?> GetStopPointId(string location)
         {
             string url = $"{baseUrl}StopPoint/Search/{Uri.EscapeDataString(location)}?app_id={appId}&app_key={appKey}";
-            var stopPointResponse = await GetFromApi<StopPointResponse>(url, "GetStopPointId");
+            var stopPointResponse = await _apiclient.GetFromApi<StopPointResponse>(url, "GetStopPointId");
 
             var bestMatch = stopPointResponse?.Matches?.FirstOrDefault(sp => sp.Modes.Contains("tube"));
             return bestMatch?.IcsId;
@@ -63,33 +65,9 @@ namespace tfl_stats.Server.Services.JourneyService
                 return [];
 
             string url = $"{baseUrl}StopPoint/Search/{Uri.EscapeDataString(query)}?app_id={appId}&app_key={appKey}";
-            var stopPointResponse = await GetFromApi<StopPointResponse>(url, "GetAutocompleteSuggestions");
+            var stopPointResponse = await _apiclient.GetFromApi<StopPointResponse>(url, "GetAutocompleteSuggestions");
 
             return stopPointResponse?.Matches?.Select(sp => sp.Name).ToList() ?? [];
         }
-
-        private async Task<T?> GetFromApi<T>(string url, string context)
-        {
-            try
-            {
-                var responseContent = await _httpclient.GetStringAsync(url);
-                return JsonConvert.DeserializeObject<T>(responseContent);
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError($"{context} - Network error: {ex.Message}");
-            }
-            catch (JsonSerializationException ex)
-            {
-                _logger.LogError($"{context} - Deserialization failed: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"{context} - Unexpected error: {ex.Message}");
-            }
-
-            return default;
-        }
-
     }
 }
