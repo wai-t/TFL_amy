43052834607506eda1c3bd7f41daee95f383c543
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-01 22:51:36 +0100

    Refactor LineService to use ApiClient

--- tfl-stats.Server/Services/LineService/LineService.cs
+++ tfl-stats.Server/Services/LineService/LineService.cs
@@ -1,37 +1,44 @@
 ﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using tfl_stats.Server.Configurations;
+using tfl_stats.Server.Models;
 using tfl_stats.Server.Models.LineModels;
 
 namespace tfl_stats.Server.Services.LineService
 {
     public class LineService
     {
-        private readonly HttpClient _httpClient;
+        //private readonly HttpClient _httpClient;
+        private readonly ApiClient _apiClient;
+
+        private ILogger<LineService> _logger;
+
         private readonly string appId;
         private readonly string appKey;
         private readonly string baseUrl;
 
-        public LineService(HttpClient httpClient, IOptions<AppSettings> options)
+        public LineService(ApiClient apiClient, IOptions<AppSettings> options, ILogger<LineService> logger)
         {
-            _httpClient = httpClient;
+            //_httpClient = httpClient;
+            this._apiClient = apiClient;
+            _logger = logger;
             appId = options.Value.appId ?? throw new ArgumentNullException(nameof(appId));
             appKey = options.Value.appKey ?? throw new ArgumentNullException(nameof(appKey));
             baseUrl = options.Value.baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
         }
 
-        public async Task<List<Line>> getLine()
+        public async Task<ResponseResult<List<Line>>> GetLine()
         {
-
-            if (string.IsNullOrEmpty(appId) || string.IsNullOrEmpty(appKey) || string.IsNullOrEmpty(baseUrl))
-            {
-                throw new InvalidOperationException("TflApi configuration is missing.");
-            }
 
             string url = $"{baseUrl}Line/Mode/tube/Status?app_id={appId}&app_key={appKey}";
 
-            var response = await _httpClient.GetStringAsync(url);
-            return JsonConvert.DeserializeObject<List<Line>>(response) ?? new List<Line>();
+            var lineResponse = await _apiClient.GetFromApi<List<Line>>(url, "GetLine");
+
+            if (lineResponse != null)
+            {
+                return new ResponseResult<List<Line>>(true, lineResponse, ResponseStatus.Ok);
+            }
+
+            return new ResponseResult<List<Line>>(false, [], ResponseStatus.NotFound);
         }
     }
 }
