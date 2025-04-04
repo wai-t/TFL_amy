1fa7c6d69f829d1ad94a7a5e19c747649e9abf1b
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-04 18:35:16 +0100

    Moving MatchedStop and StopPointSearchResponse classes to Search folder

---
+++ tfl-stats.Server/Models/StopPointModels/Search/StopPointSearchResponse.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using tfl_stats.Server.Models.JourneyModels;
+
+namespace tfl_stats.Server.Models.StopPointModels
+{
+    public class StopPointSearchResponse
+    {
+        [JsonProperty("matches")]
+        [JsonRequired]
+        public List<MatchedStop> Matches { get; set; } = new List<MatchedStop>();
+    }
+}
