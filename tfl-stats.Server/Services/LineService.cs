9b592cae24749e79046aac66896c1352ab51f229
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-04 18:27:59 +0100

    Introduces the `LineService` class in the `tfl_stats.Server.Services` namespace

---
+++ tfl-stats.Server/Services/LineService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using tfl_stats.Server.Client;
+using tfl_stats.Server.Configurations;
+using tfl_stats.Server.Models;
+using tfl_stats.Server.Models.LineModels;
+
+namespace tfl_stats.Server.Services
+{
+    public class LineService
+    {
+        private readonly ApiClient _apiClient;
+
+        private ILogger<LineService> _logger;
+
+        private readonly string appId;
+        private readonly string appKey;
+        private readonly string baseUrl;
+
+        public LineService(ApiClient apiClient, IOptions<AppSettings> options, ILogger<LineService> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+            appId = options.Value.appId ?? throw new ArgumentNullException(nameof(appId));
+            appKey = options.Value.appKey ?? throw new ArgumentNullException(nameof(appKey));
+            baseUrl = options.Value.baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
+        }
+
+        public async Task<ResponseResult<List<Line>>> GetLine()
+        {
+
+            string url = $"{baseUrl}Line/Mode/tube/Status?app_id={appId}&app_key={appKey}";
+
+            var lineResponse = await _apiClient.GetFromApi<List<Line>>(url, "GetLine");
+
+            if (lineResponse != null)
+            {
+                return new ResponseResult<List<Line>>(true, lineResponse, ResponseStatus.Ok);
+            }
+
+            return new ResponseResult<List<Line>>(false, new List<Line>(), ResponseStatus.NotFound);
+        }
+    }
+}
