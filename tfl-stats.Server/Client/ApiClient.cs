5a8d2ed848e276abcdf14380a0417a2884dd3c71
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-04-04 18:41:05 +0100

    Add ApiClient class for API requests and error handling

---
+++ tfl-stats.Server/Client/ApiClient.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace tfl_stats.Server.Client
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<ApiClient> _logger;
+
+        public ApiClient(HttpClient httpClient, ILogger<ApiClient> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task<T?> GetFromApi<T>(string url, string context)
+        {
+            try
+            {
+                var responseContent = await _httpClient.GetStringAsync(url);
+                //_logger.LogInformation("Raw API Response for {Context}: {Response}", context, responseContent);
+                return JsonConvert.DeserializeObject<T>(responseContent);
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError($"{context} - Network error: {ex.Message}");
+            }
+            catch (JsonSerializationException ex)
+            {
+                _logger.LogError($"{context} - Deserialization failed: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{context} - Unexpected error: {ex.Message}");
+            }
+
+            return default;
+        }
+    }
+}
